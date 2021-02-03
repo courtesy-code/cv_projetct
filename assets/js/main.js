11e54faae424beebae19a61ad13412bcb26e994c
Raphael "courtesy" Garcia <77281371+courtesy-code@users.noreply.github.com>
2021-02-03 18:55:45 -0300

    Update Nav_bar
    Update JS nav_bar.

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,20 +15,19 @@ const navLink = document.querySelectorAll('.nav__link')
 function linkAction(){
     navMenu.classList.remove('show')
 }
-navLink.forEach(n => n.addEventListener('click, linkAction'))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-const sections = document.querySelectorAll('selection[id]')
+const sections = document.querySelectorAll('section[id]')
 
-window.addEventListener('scroll, scrollActive')
+window.addEventListener('scroll', scrollActive)
 
 function scrollActive(){
     const scrollY = window.pageYOffset
-
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute('id')
-
+        
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
         } else{
@@ -36,3 +35,4 @@ function scrollActive(){
         }
     })
 }
+
